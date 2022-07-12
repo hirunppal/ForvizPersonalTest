988d7354475e26d09b527c2d4120e81c3beb9e96
hirunppal <hirunppal@gmail.com>
2022-07-12 13:29:57 +0700

    feat:css-5-5

--- js_Test/app.js
+++ js_Test/app.js
@@ -1,25 +1,17 @@
 const demoBooking = require('./demo-booking-data.json');
 
-// console.log('hi');
-// console.log(demoBooking);
-
 const checkAvailability = (roomId, startTime, endTime) => {
   const targetRooms = demoBooking.filter((el) => {
     if (el.roomId === roomId) return el;
-
-    // const Status=
   });
   // console.log(targetRooms);
-
   const Checklog = [];
   targetRooms.forEach((ele) => {
     const roomTimestart = new Date(ele.startTime).getTime();
     const roomTimeEnd = new Date(ele.endTime).getTime();
     const expectTimestart = new Date(startTime).getTime();
     const expectTimeEnd = new Date(endTime).getTime();
-
     if (roomTimeEnd <= expectTimestart || roomTimestart >= expectTimeEnd) {
-      // return
     } else return Checklog.push(ele.title);
   });
   // console.log(new Date(targetRoom.startTime).getTime());
@@ -28,24 +20,66 @@ const checkAvailability = (roomId, startTime, endTime) => {
   // console.log(new Date(startTime).getTime());
   // console.log(Checklog);
   if (Checklog?.length < 1) {
-    return (
-      'The room is available if there’re no other current bookings during ' +
-      startTime +
-      ' - ' +
-      endTime
-    );
+    return {
+      status: true,
+      massage:
+        'The room is available if there’re no other current bookings during ' +
+        startTime +
+        ' - ' +
+        endTime,
+    };
   } else {
-    return { massage: 'room is Not available ', room_bookings: targetRooms };
+    return {
+      status: false,
+      massage: 'room is Not available ',
+      room_bookings: targetRooms,
+    };
   }
 };
-// return true / false;
+
+// test return true / false;
 const awns = checkAvailability(
   'A102',
   '2017-09-28 16:00:01',
   '2019-09-30 16:00:02'
 );
-
 // console.log(awns);
+
 const getBookingsForWeek = (roomId, weekNo) => {
-  return [];
+  const today = '' + new Date();
+  const weekday = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  const startDay = (obj) => '' + new Date(obj.startTime);
+
+  const options = {
+    1: (el) => {
+      const todaytext = '' + new Date('2019-10-01 10:00:00');
+      // console.log(today);
+      // console.log(today.slice(0, 16));
+      console.log(startDay(el).slice(0, 16));
+      if (today.slice(0, 16) === startDay(el).slice(0, 16)) {
+        return el;
+      }
+    },
+    2: (elthisweek) => {
+      console.log(startDay(elthisweek));
+      if (today.getDay() < startDay(elthisweek).getDay() && true) {
+      }
+      return elthisweek;
+      // const
+    },
+    // 3: next_week,
+  };
+  // const ArrofBookings = [];
+  // for (let i = weekNo; i > 0; i--) {
+  //   if (i > 3) console.log(i);
+  // }
+
+  const roomBookings = demoBooking.filter((el) => el.roomId === roomId);
+  console.log(options[weekNo]);
+  return weekNo
+    ? roomBookings.filter(options[weekNo])
+    : Object.values(options).map((e) => roomBookings.filter(e));
 };
+
+//test
+console.log(getBookingsForWeek('A102', 1));
