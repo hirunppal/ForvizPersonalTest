896c4626a673c83f7c37f981f7ddb7d203fc253c
hirunppal <hirunppal@gmail.com>
2022-07-14 17:32:35 +0700

    feat:css5/5js5/5

--- front_Test/my_app/src/App.js
+++ front_Test/my_app/src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import BookingLayout from './Pages/BookingLayout';
+import DynamicBookingLists from './Component/DynamicBookingLists';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className=' relative '>
+        <div className='w-full h-screen absolute top-0 flex justify-center items-center first-letter rectangle '>
+          <Routes>
+            <Route path='/booking' element={<BookingLayout />}>
+              <Route path='thisweek' element={<DynamicBookingLists />} />
+              <Route path='nextweek' element={<DynamicBookingLists />} />
+              <Route path='wholemonth' element={<DynamicBookingLists />} />
+              <Route
+                path='*'
+                element={<Navigate to='thisweek?roomId=A101'></Navigate>}
+              />
+            </Route>
+            <Route path='*' element={<Navigate to='/booking'></Navigate>} />
+          </Routes>
+        </div>
+      </div>
     </div>
   );
 }
--- js_Test/app.js
+++ js_Test/app.js
@@ -14,11 +14,6 @@ const checkAvailability = (roomId, startTime, endTime) => {
     if (roomTimeEnd <= expectTimestart || roomTimestart >= expectTimeEnd) {
     } else return Checklog.push(ele.title);
   });
-  // console.log(new Date(targetRoom.startTime).getTime());
-  // console.log(new Date(startTime).getTime());
-  // console.log(new Date(targetRoom.startTime).getTime());
-  // console.log(new Date(startTime).getTime());
-  // console.log(Checklog);
   if (Checklog?.length < 1) {
     return {
       status: true,
@@ -37,30 +32,13 @@ const checkAvailability = (roomId, startTime, endTime) => {
   }
 };
 
-// test return true / false;
-const awns = checkAvailability(
-  'A102',
-  '2017-09-28 16:00:01',
-  '2019-09-30 16:00:02'
-);
-// console.log(awns);
-//
-//
-///
-///
-//
 const getBookingsForWeek = (roomId, weekNo) => {
-  // const today = new Date();
   const today = new Date('2019-09-30 09:00:00');
   const weekday = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
   const startDay = (obj) => new Date(obj.startTime);
 
   const GetfirstdateOftheWeek = (fulldate) =>
     new Date(fulldate.setDate(fulldate.getDate() - fulldate.getDay()));
-  // const GetfirstdateOfnextWeek = (fulldate) =>
-  // new Date(
-  //   fulldate.setDate(fulldate.getDate() - fulldate.getDay())
-  // ).getTime();
   const mstoday = 1000 * 60 * 60 * 24;
   const optionsWeekno = {
     1: (el) => {
@@ -68,8 +46,6 @@ const getBookingsForWeek = (roomId, weekNo) => {
         return el;
       }
     },
-    //fuction for get this week Books
-
     2: (elthisweek) => {
       const firstdate = GetfirstdateOftheWeek(today).getTime() / mstoday;
       const lastdate =
@@ -79,23 +55,14 @@ const getBookingsForWeek = (roomId, weekNo) => {
           )
         ) / mstoday;
 
-      console.log('Math.floor(firstdate)');
-      console.log(startDay(elthisweek).getTime() / mstoday);
-      console.log(Math.floor(firstdate));
-      // console.log(REALnextweekToday.getTime() / mstoday);
-      console.log(Math.ceil(lastdate));
-
       if (
         Math.floor(firstdate) <= startDay(elthisweek).getTime() / mstoday &&
         startDay(elthisweek).getTime() / mstoday <= Math.ceil(lastdate)
       ) {
         return elthisweek;
       }
-      // const
     },
     3: (searchextWeek) => {
-      // console.log(searchextWeek);
-      // .getTime() / mstoday
       const todayToNextWeek = new Date(today.getTime());
       const REALnextweekToday = new Date(
         todayToNextWeek.setDate(todayToNextWeek.getDate() + 7)
@@ -106,7 +73,6 @@ const getBookingsForWeek = (roomId, weekNo) => {
       const NextWeekstartDay = new Date(
         nextweekdate.setDate(nextweekdate.getDate())
       );
-      // .getDate();
       const date = new Date(GetfirstdateOftheWeek(NextWeekstartDay).getTime());
       const firstdatee =
         new Date(
@@ -118,10 +84,6 @@ const getBookingsForWeek = (roomId, weekNo) => {
           NextWeekstartDay.setDate(NextWeekstartDay.getDate() + 6)
         ).getTime() / mstoday;
 
-      // console.log(searchextWeek);
-      // console.log(Math.floor(firstdatee));
-      // console.log(REALnextweekToday.getTime() / mstoday);
-      // console.log(Math.ceil(lastDateOfnextWeek));
       if (
         Math.floor(firstdatee) <= REALnextweekToday.getTime() / mstoday &&
         REALnextweekToday.getTime() / mstoday <= Math.ceil(lastDateOfnextWeek)
@@ -133,8 +95,6 @@ const getBookingsForWeek = (roomId, weekNo) => {
 
   const thisRoomBookings = demoBooking.filter((el) => el.roomId === roomId);
 
-  // console.log(Object.values(optionsWeekno).map((e) => e));
-
   if (weekNo) {
     const getBookingsForWeek = thisRoomBookings.filter(optionsWeekno[weekNo]);
     if (getBookingsForWeek?.length > 0) {
@@ -142,7 +102,6 @@ const getBookingsForWeek = (roomId, weekNo) => {
     } else return 'none';
   } else {
     return Object.values(optionsWeekno).map((e, idx) => {
-      // console.log(thisRoomBookings.filter(e));
       return {
         For:
           idx === 0
@@ -159,7 +118,20 @@ const getBookingsForWeek = (roomId, weekNo) => {
 };
 
 //test
+// console.log('CheckRoombyBookingTime');
+// const awns = checkAvailability(
+//   'A102',
+//   '2017-09-28 16:00:01',
+//   '2019-09-30 16:00:02'
+// );
+// console.log(awns)
 
-console.log('BookingForTheRooom');
-console.log(JSON.stringify(getBookingsForWeek('A102')));
-//
+/////////////////
+
+// console.log('CheckRoombyRoomId');
+// console.log(JSON.stringify(getBookingsForWeek('A102', 1)));
+// console.log(JSON.stringify(getBookingsForWeek('A102', 2)));
+// console.log(JSON.stringify(getBookingsForWeek('A102', 3)));
+// console.log(JSON.stringify(getBookingsForWeek('A102')));
+
+export { checkAvailability, getBookingsForWeek };
